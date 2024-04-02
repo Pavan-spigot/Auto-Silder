81f45e11475cbbc4cb1689adc8e9773d641e9216
Pavan-spigot <165754072+Pavan-spigot@users.noreply.github.com>
2024-04-02 18:34:37 +0530

    commit messsage

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+var swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  direction: getDirection(),
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  on: {
+    resize: function () {
+      swiper.changeDirection(getDirection());
+    },
+  },
+});
+
+function getDirection() {
+  var windowWidth = window.innerWidth;
+  var direction = window.innerWidth <= 1000 ? 'vertical' : 'horizontal';
+
+  return direction;
+}
